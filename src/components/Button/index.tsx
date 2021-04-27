a39fc0b49b15d245eaa9289e24d9b81340a2c57e
charlespereira1 <charlesti.ptu@gmail.com>
2021-04-27 00:03:44 -0300

    components button configure

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { TouchableOpacityProps } from "react-native";
+
+import { Container, ButtonText } from "./styles";
+
+interface ButtonProps extends TouchableOpacityProps {
+  title: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ title, ...rest }) => {
+  return (
+    <Container {...rest}>
+      <ButtonText>{title}</ButtonText>
+    </Container>
+  );
+};
+
+export default Button;
