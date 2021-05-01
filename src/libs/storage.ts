cb03fa07d86bb8fd3709600fcb000bd570671fb2
charlespereira1 <charlesti.ptu@gmail.com>
2021-05-01 00:52:40 -0300

    refecture folders

---
+++ src/libs/storage.ts
@@ -0,0 +1,4 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { PlantsProps } from "./types";
+
+export const savePlant = async (plant: PlantsProps): Promise<void> => {};
---
+++ src/libs/types.ts
@@ -0,0 +1,23 @@
+export interface EnviromentsProps {
+  key: string;
+  title: string;
+}
+
+export interface PlantsProps {
+  id: string;
+  name: string;
+  about: string;
+  water_tips: string;
+  photo: string;
+  environments: [string];
+  frequency: {
+    times: number;
+    repeat_every: Date;
+  };
+}
+
+export interface StoragePlantProps {
+  [id: string]: {
+    data: PlantsProps;
+  };
+}
