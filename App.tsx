125f353a2157614ee7e79977ce7c9ea6f0d87819
Charles Pereira <charlesti.ptu@gmail.com>
2021-05-10 23:11:33 -0300

    configure notifications mobile app.tsx

--- App.tsx
+++ App.tsx
@@ -1,17 +1,30 @@
 import "react-native-gesture-handler";
 
-import React from "react";
+import React, { useEffect } from "react";
 import AppLoading from "expo-app-loading";
 import {
   useFonts,
   Jost_400Regular,
   Jost_600SemiBold,
 } from "@expo-google-fonts/jost";
+import * as Notifications from "expo-notifications";
 
 import Routes from "./src/routes";
 
+import { PlantsProps } from "./src/libs/types";
+
 const App: React.FC = () => {
   const [fontsLoaded] = useFonts({ Jost_400Regular, Jost_600SemiBold });
+
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(
+      async (notification) => {
+        const data = notification.request.content.data.plant as PlantsProps;
+      }
+    );
+
+    return () => subscription.remove();
+  }, []);
 
   if (!fontsLoaded) {
     return <AppLoading />;
